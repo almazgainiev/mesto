072438bfb19354bb1d3acad085ee2a535c955081
Almaz Gainiev <gainievalm@yandex.ru>
2023-01-05 15:01:44 +0300

    mesto d

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__redact-btn');
-console.log(popupOpenButtonElement);
 const openPopup = function () {
   popupElement.classList.add('popup_opened');
 }
@@ -15,9 +14,19 @@ const closePopupByClickOnOverlay = function (event) {
 
   closePopup();
 }
-
-/**addPopupVisibility();**/
-
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByClickOnOverlay);
+
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.form__item_type_name');
+let jobInput = formElement.querySelector('.form__item_type_job');
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+let getValueOfName = document.querySelector('.form__item_type_name').value;
+let getValueOfJob = document.querySelector('.form__item_type_job').value;
+document.querySelector('.profile__name').textContent = getValueOfName;
+document.querySelector('.profile__description').textContent = getValueOfJob;
+}
+formElement.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener('submit', closePopup);
